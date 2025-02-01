c2e4a4faa5fc38dfa6578e708c599bc4222bc5d4
Susovan12 <102346168+Susovan12@users.noreply.github.com>
2025-02-01 14:40:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -37,19 +37,19 @@ async function checkWeather(city){
 
     switch(weather_data.weather[0].main){
         case 'Clouds':
-            weather_img.src = "https://i.imghippo.com/files/bOE7941zE.png";
+            weather_img.src = "cloud.png";
             break;
         case 'Clear':
-            weather_img.src = "https://i.imghippo.com/files/ShyU3803Xco.png";
+            weather_img.src = "clear.png";
             break;
         case 'Rain':
-            weather_img.src = "/Weather App/rain.png";
+            weather_img.src = "rain.png";
             break;
         case 'Mist':
-            weather_img.src = "https://i.imghippo.com/files/JCbS6499dSI.png";
+            weather_img.src = "mist.png";
             break;
         case 'Snow':
-            weather_img.src = "https://i.imghippo.com/files/WBT8619uwg.png";
+            weather_img.src = "snow.png";
             break;
 
     }
